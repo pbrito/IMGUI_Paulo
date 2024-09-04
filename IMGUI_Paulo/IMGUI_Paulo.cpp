3df4d00f5f69f28c348ff5571b14c7b2ac9ce481
pbrito <pbrito@gmail.com>
2024-09-04 17:45:50 +0100

    Add OpenGL initialization and triangle drawing
    - Included GLAD library for OpenGL function loading.
    - Added function declarations for OpenGL setup and drawing.
    - Modified `DrawImGuiContent()` to control triangle drawing.
    - Added shader source code for vertex and fragment shaders.
    - Updated `main()` to initialize OpenGL, create shaders, and draw triangle.
    - Added ImGui checkbox to toggle triangle drawing.
    - Cleaned up OpenGL resources before terminating GLFW.

--- IMGUI_Paulo/IMGUI_Paulo.cpp
+++ IMGUI_Paulo/IMGUI_Paulo.cpp
@@ -4,15 +4,43 @@
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 // Function declarations
 GLFWwindow* InitializeGLFW(int width, int height, const char* title);
+void initializeOpenGL();
+GLuint createShaderProgram();
+void setupVertexData(GLuint& VAO, GLuint& VBO);
 void InitializeImGui(GLFWwindow* window);
 void CleanupImGui();
 void RenderFrame();
-void DrawImGuiContent();
+void DrawImGuiContent(bool & drawTriangle);
 void NewFrame();
+void SetupTriangle(GLuint& VAO, GLuint& VBO, GLuint& shaderProgram);
+void DrawTriangle(GLuint VAO, GLuint shaderProgram);
+
+
+#pragma region shader Source
+// Vertex Shader source code
+const char* vertexShaderSource = R"(
+    #version 330 core
+    layout (location = 0) in vec3 aPos;
+    void main()
+    {
+        gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    }
+)";
+// Fragment Shader source code
+const char* fragmentShaderSource = R"(
+    #version 330 core
+    out vec4 FragColor;
+    void main()
+    {
+        FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    }
+)";
+#pragma endregion
 
 
 int main()
@@ -21,13 +49,22 @@ int main()
     if (!window)
         return -1;
 
+    initializeOpenGL();
+    GLuint shaderProgram = createShaderProgram();
+    GLuint VAO, VBO;
+    setupVertexData(VAO, VBO);
+    SetupTriangle(VAO, VBO, shaderProgram);
+
+
     // Initialize ImGui
     InitializeImGui(window);
-
+    
 
     // Main loop
     while (!glfwWindowShouldClose(window))
     {
+        // Variables for ImGui controls
+        static bool drawTriangle= false;
         // Poll events
         glfwPollEvents();
 
@@ -35,10 +72,13 @@ int main()
 		glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        // Draw the triangle
+        if(drawTriangle)
+        DrawTriangle(VAO, shaderProgram);
 
         NewFrame();
 
-        DrawImGuiContent();
+        DrawImGuiContent(drawTriangle);
 
         RenderFrame();
 
@@ -49,6 +89,9 @@ int main()
 
     // Cleanup
     CleanupImGui();
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
     glfwTerminate();
     return 0;
 }
@@ -113,11 +156,14 @@ void RenderFrame()
 
 }
 
-void DrawImGuiContent()
+void DrawImGuiContent( bool &drawTriangle)
 {
     //render the imgui window
     ImGui::Begin("Hello, world!");
     ImGui::Text("This is some useful text.");
+//Imgui checkbox for bool drawTraingle
+	ImGui::Checkbox("Draw Triangle", &drawTriangle);
+
     ImGui::End();
 }
 
@@ -126,4 +172,109 @@ void NewFrame()
     ImGui_ImplOpenGL3_NewFrame();
     ImGui_ImplGlfw_NewFrame();
     ImGui::NewFrame();
+}
+
+
+// Setup the triangle (VAO, VBO, and shader program)
+void SetupTriangle(GLuint& VAO, GLuint& VBO, GLuint& shaderProgram)
+{
+    // Set up vertex data and buffers
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f
+    };
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // Create and compile shaders
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    // Create shader program
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+}
+
+// Draw the triangle
+void DrawTriangle(GLuint VAO, GLuint shaderProgram)
+{
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+}
+
+// Function to initialize OpenGL   
+void initializeOpenGL()
+{
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cerr << "Failed to initialize GLAD" << std::endl;
+        exit(-1);
+    }
+    glViewport(0, 0, 800, 800);
+}
+
+// Function to create a shader program
+GLuint createShaderProgram()
+{
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+    glCompileShader(vertexShader);
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+    glCompileShader(fragmentShader);
+
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    return shaderProgram;
+}
+
+// Function to setup vertex data
+void setupVertexData(GLuint& VAO, GLuint& VBO)
+{
+    float vertices[] = {
+        -0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
+        0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
+        0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f
+    };
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
 }
